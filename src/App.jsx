78c2f41d08f0c38a9c0060842572fcfac625d480
Gentlecoder1 <israeloloruntoba3@gmail.com>
2025-12-12 21:00:54 +0100

    feat: implemented internal window management functionality with Zustand for FinderWindowand created the FinderWindow

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Navbar, Welcome, Dock } from "#components"
-import { Safari, Terminal, Resume } from "#windows";
+import { Safari, Terminal, Resume, Finder } from "#windows";
 
 // import gsap from "gsap";
 // import { Draggable } from "gsap/Draggable";
@@ -16,6 +16,7 @@ function App() {
       <Safari />
       <Terminal />
       <Resume />
+      <Finder />
     </main>
   )
 }
